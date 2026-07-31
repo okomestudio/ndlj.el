4674cf1f0748de0658e9051a395b8a1be7f95456
Taro Sato <okomestudio@gmail.com>
2026-07-30 18:31:11 -0700

    Fix a bug not using whitespace as delimiter

--- ndlj-openurl.el
+++ ndlj-openurl.el
@@ -320,7 +320,7 @@ When given, CREATOR-INDICES holds creator index (著者標目) entries."
     (append
      (mapcar
       (lambda (str)
-        (cdr (string-split str "\\([.] \\|--\\)" t "\\s-+")))
+        (cdr (string-split str "\\([.]? \\|--\\)" t "\\s-+")))
       (append (when (map-elt rec "NDC9版") `(,(map-elt rec "NDC9版")))
               (when (map-elt rec "NDC10版") `(,(map-elt rec "NDC10版")))))
      (mapcar
@@ -330,7 +330,7 @@ When given, CREATOR-INDICES holds creator index (著者標目) entries."
               ((and (map-elt it "氏") (map-elt it "名"))
                (concat (map-elt it "氏") " " (map-elt it "名")))
               (t
-               (string-split (map-elt it "件名") "--" 'omit-empty "\\s-+"))))
+               (string-split (map-elt it "件名") " -- " 'omit-empty "\\s-+"))))
       (map-elt rec "件名標目"))))))
 
 ;;;###autoload
